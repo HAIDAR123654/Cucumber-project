bad2b480c0885d4b98eecad744450d37f178e983
ha890 <ha890@MY-LAPTOP>
2022-08-21 08:25:08 +0530

    Second commit

---
+++ Excel_data/src/characterCount/CharacterOrderNotMatter.java
@@ -0,0 +1,28 @@
+package characterCount;
+import java.util.*;
+
+public class CharacterOrderNotMatter {
+	public static void logic(String s) {
+		Map<Character,Integer> map = new HashMap<>();
+		
+		for(int i=0; i<s.length(); i++) {
+			if(map.containsKey(s.charAt(i))) {
+				map.put(s.charAt(i), map.get(s.charAt(i))+1);
+			}
+			else {
+				map.put(s.charAt(i), 1);
+			}
+		}
+		for(Map.Entry<Character, Integer> e: map.entrySet()) {
+			System.out.println(e.getKey() + " " + e.getValue());
+		}
+		
+	}
+
+	public static void main(String[] args) {
+		String s = "Haidar";
+		logic(s);
+
+	}
+
+}
